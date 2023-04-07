fa4ea1cc2ce834a7e693992557e1470f66a236be
greenhandzpx <893522573@qq.com>
2023-04-07 20:28:11 +0800

    pass ethernet

--- src/ethernet.c
+++ src/ethernet.c
@@ -11,6 +11,14 @@
 void ethernet_in(buf_t *buf)
 {
     // TO-DO
+    if (buf->len < sizeof(ether_hdr_t)) {
+        return;
+    }
+    ether_hdr_t *hdr = (ether_hdr_t *)buf->data;
+    uint16_t protocol = swap16(hdr->protocol16);
+
+    buf_remove_header(buf, sizeof(ether_hdr_t));
+    net_in(buf, protocol, hdr->src);
 }
 /**
  * @brief 处理一个要发送的数据包
@@ -22,6 +30,16 @@ void ethernet_in(buf_t *buf)
 void ethernet_out(buf_t *buf, const uint8_t *mac, net_protocol_t protocol)
 {
     // TO-DO
+    if (buf->len < ETHERNET_MIN_TRANSPORT_UNIT) {
+        buf_add_padding(buf, ETHERNET_MIN_TRANSPORT_UNIT - buf->len);
+    }
+    buf_add_header(buf, sizeof(ether_hdr_t));
+    ether_hdr_t *hdr = (ether_hdr_t *)buf->data;
+    memcpy(hdr->dst, mac, NET_MAC_LEN);
+    uint8_t src[NET_MAC_LEN] = NET_IF_MAC;
+    memcpy(hdr->src, src, NET_MAC_LEN);
+    hdr->protocol16 = swap16(protocol);
+    driver_send(buf);
 }
 /**
  * @brief 初始化以太网协议
